e583c5f073cab0bf670e177caca28fb868affb78
Eduardo Marcelino <eduardoofficial12@gmail.com>
2025-02-22 23:09:50 -0300

    Adicionando ultimas funcionalidades, 404.css and 404.html

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from database import database
 import views
 
@@ -9,6 +9,11 @@ app.route("/notes", methods=["GET"])(views.get_notes)
 app.route("/note", methods=["POST"])(views.add_note)  
 app.route("/note/<int:note_id>", methods=["PUT"])(views.update_note)
 app.route("/note/<int:note_id>", methods=["DELETE"])(views.delete_note)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     database.create_table_notes()
